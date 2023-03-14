0ac89c5568310c5b7cf3bfb25294fb5956e88040
CatJelly <mej1406@gmail.com>
2023-03-14 13:41:05 +0900

    lecture 13

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -1,12 +1,24 @@
 package com.ll.basic1;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.*;
 
 @Controller
 public class HomeController {
-    public int count = 0;
+    private int count;
+    private List<Person> list = null;
+
+    public HomeController() {
+        count = 0;
+        list = new ArrayList<>();
+    }
 
     @GetMapping("/home/main")
     @ResponseBody
@@ -28,7 +40,34 @@ public class HomeController {
 
     @GetMapping("/home/increase")
     @ResponseBody
-    public String increase() {
-        return count++ + "";
+    public int showIncrease() {
+        return count++;
     }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    public int showPlus(@RequestParam(defaultValue = "0") int a, int b) {
+        return a + b;
+    }
+
+    @GetMapping("/home/addPerson")
+    @ResponseBody
+    public String addPerson(@RequestParam String name, int age) {
+        list.add(new Person(++count, name, age));
+        return String.format("%d번 사람이 추가되었습니다.", list.size());
+    }
+
+    @GetMapping("/home/people")
+    @ResponseBody
+    public List<Person> people() {
+        return list;
+    }
+}
+
+@AllArgsConstructor
+@Getter
+class Person {
+    int id;
+    String name;
+    int age;
 }
